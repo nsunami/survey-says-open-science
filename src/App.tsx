b1090479a0df8c6ec6678f74ffae199d5d2fa669
Nami Sunami <nsunami@pm.me>
2025-04-14 17:04:40 +0200

    fix: Reset answer choices when navigating to the next question

--- src/App.tsx
+++ src/App.tsx
@@ -2,26 +2,26 @@ import { useMemo, useState } from "react"
 import { questions } from "./data/sample-questions"
 
 function App() {
-  const [currentQuestion, setCurrentQuestion] = useState(0)
-  const [answers, setAnswers] = useState(questions[currentQuestion].choices)
+  const [currentQuestionId, setCurrentQuestionId] = useState(0)
+  const [answers, setAnswers] = useState(questions[currentQuestionId].choices)
   const [totalScore, setTotalScore] = useState(0)
   const [strikes, setStrikes] = useState(0)
 
   const title = useMemo(
-    () => questions[currentQuestion].text,
-    [currentQuestion]
+    () => questions[currentQuestionId].text,
+    [currentQuestionId]
   )
 
   const handleNextQuestion = () => {
-    setCurrentQuestion((current: number) => {
-      if (current >= questions.length - 1) {
-        setCurrentQuestion(0)
-        return 0
-      }
-      return current + 1
-    })
-    setAnswers(questions[currentQuestion + 1].choices)
     setStrikes(0)
+    setCurrentQuestionId((current: number) => {
+      if (current >= questions.length - 1) {
+        return current
+      }
+      const nextQuestionId = current + 1
+      setAnswers(questions[nextQuestionId].choices)
+      return nextQuestionId
+    })
   }
 
   const handleReveal = (id: number) => {
@@ -46,6 +46,18 @@ function App() {
     setAnswers(answers.map((answer) => ({ ...answer, revealed: false })))
     setTotalScore(0)
     setStrikes(0)
+  }
+
+  const revealAll = () => {
+    setAnswers(
+      answers.map((answer) => {
+        if (!answer.revealed) {
+          setTotalScore(totalScore + answer.points)
+          return { ...answer, revealed: true }
+        }
+        return answer
+      })
+    )
   }
 
   return (
@@ -102,21 +114,27 @@ function App() {
         <div className="flex justify-center space-x-4">
           <button
             onClick={handleStrike}
-            className="bg-red-600 hover:bg-red-700 px-6 py-2 rounded-lg font-bold"
+            className="bg-red-600 hover:bg-red-700 px-6 py-2 rounded-lg font-bold hover:cursor-pointer"
           >
             Strike
           </button>
           <button
             onClick={resetGame}
-            className="bg-green-600 hover:bg-green-700 px-6 py-2 rounded-lg font-bold"
+            className="bg-slate-600 hover:bg-slate-700 px-6 py-2 rounded-lg font-bold hover:cursor-pointer"
           >
             Reset
           </button>
         </div>
         <div className="flex justify-center space-x-4 my-4">
           <button
+            onClick={revealAll}
+            className="bg-slate-600 hover:bg-slate-700 px-6 py-2 rounded-lg font-bold hover:cursor-pointer"
+          >
+            Reveal All
+          </button>
+          <button
             onClick={handleNextQuestion}
-            className="bg-green-600 hover:bg-green-700 px-6 py-2 rounded-lg font-bold hover:cursor-pointer"
+            className="bg-slate-600 hover:bg-slate-700 px-6 py-2 rounded-lg font-bold hover:cursor-pointer"
           >
             Next Question
           </button>
