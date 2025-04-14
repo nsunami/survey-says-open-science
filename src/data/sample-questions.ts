12598f3024f6c44b04ac098f6443025af5bbb569
Nami Sunami <nsunami@pm.me>
2025-04-14 16:41:32 +0200

    feat: Add sample questions

---
+++ src/data/sample-questions.ts
@@ -0,0 +1,64 @@
+export const questions = [
+  {
+    id: 1,
+    text: "Open Science, just science ____",
+    choices: [
+      { id: 1, text: "done right", points: 31, revealed: false },
+      { id: 2, text: "for everyone", points: 19, revealed: false },
+      { id: 3, text: "made transparent", points: 12, revealed: false },
+      { id: 4, text: "shared widely", points: 10, revealed: false },
+      { id: 5, text: "without paywalls", points: 8, revealed: false },
+      { id: 6, text: "in the open", points: 6, revealed: false },
+      { id: 7, text: "democratized", points: 5, revealed: false },
+      { id: 8, text: "as it should be", points: 4, revealed: false },
+      { id: 9, text: "with integrity", points: 3, revealed: false },
+      { id: 10, text: "reimagined", points: 2, revealed: false },
+    ],
+  },
+  {
+    id: 2,
+    text: "The most hurtful consequence of Gold Open Access is ____",
+    choices: [
+      { id: 1, text: "high publication fees", points: 31, revealed: false },
+      { id: 2, text: "predatory journals", points: 19, revealed: false },
+      {
+        id: 3,
+        text: "financial barriers for researchers",
+        points: 12,
+        revealed: false,
+      },
+      {
+        id: 4,
+        text: "inequality between institutions",
+        points: 10,
+        revealed: false,
+      },
+      {
+        id: 5,
+        text: "profit-driven publishing model",
+        points: 8,
+        revealed: false,
+      },
+      {
+        id: 6,
+        text: "exclusion of researchers from developing countries",
+        points: 6,
+        revealed: false,
+      },
+      { id: 7, text: "quality concerns", points: 5, revealed: false },
+      {
+        id: 8,
+        text: "unsustainable funding models",
+        points: 4,
+        revealed: false,
+      },
+      {
+        id: 9,
+        text: "academic career disadvantages",
+        points: 3,
+        revealed: false,
+      },
+      { id: 10, text: "publisher monopolies", points: 2, revealed: false },
+    ],
+  },
+]
